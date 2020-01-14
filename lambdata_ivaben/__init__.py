fa07f9801f91dad2a72d0219cb1ba62b8abcb77b
ivaben <ivaben@users.noreply.github.com>
2020-01-14 18:27:54 -0500

    uploading chnges to init file

--- lambdata_ivaben/__init__.py
+++ lambdata_ivaben/__init__.py
@@ -1,5 +1,13 @@
-@echo off
-echo Hello this a test batch file
-pause
-dir c:\windows
-pause
+
+
+"""
+lambdata - a collection of data science helper functions
+"""
+
+import pandas as  pd
+import numpy as np
+
+# sample code
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
